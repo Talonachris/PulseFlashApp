bae1cef97f326f6eb3cde9a012c7329f65ce9636
Talonachris <chris@nebuliton.de>
2025-07-12 04:14:22 +0200

    Initial Commit with app icons and beginning of the projekt

---
+++ PulseFlash/PulseFlash/PulseFlashApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulseFlashApp.swift
+//  PulseFlash
+//
+//  Created by Christian Hagenacker on 12.07.25.
+//
+
+import SwiftUI
+
+@main
+struct PulseFlashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
